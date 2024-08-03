841b3df73b3a933d5da4017297e82f542dd99805
mnsxyz <ms7eo69@gmail.com>
2024-08-03 17:33:19 +0900

    Dao CUD 메소드 추가

--- app/src/main/java/com/example/cafe/dao/Dao.kt
+++ app/src/main/java/com/example/cafe/dao/Dao.kt
@@ -20,10 +20,28 @@ class Dao {
 
     suspend inline fun <reified T : Any> findOne(id: Long): T {
         return client.postgrest.from(TableNameGetter.get<T>())
-            .select() {
-                filter { eq("id", id) }
-            }
+            .select { filter { eq("id", id) } }
             .decodeSingle()
+    }
+
+    suspend inline fun <reified T : Any> insert(t: T) {
+        client.postgrest.from(TableNameGetter.get<T>())
+            .insert(t)
+    }
+
+    suspend inline fun <reified T : Any> update(t: T) {
+        client.postgrest.from(TableNameGetter.get<T>())
+            .update(t)
+    }
+
+    suspend inline fun <reified T : Any> upsert(t: T) {
+        client.postgrest.from(TableNameGetter.get<T>())
+            .upsert(t)
+    }
+
+    suspend inline fun <reified T : Any> delete(id: Long) {
+        client.postgrest.from(TableNameGetter.get<T>())
+            .delete { filter { eq("id", id) } }
     }
 }
 
